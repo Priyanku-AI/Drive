989f567546783b2836b952ea90146f3f45b617f3
Priyanku Saikia <paulsaikia6@gmail.com>
2025-04-20 02:36:17 +0530

    put await on userModel.create

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
+const userModel = require('../models/user.model');   
 
 router.get("/test", (req, res) => {
   res.send("user Test route");
@@ -19,14 +20,28 @@ router.post(
   body("email").trim().isEmail().isLength({ min: 13 }),
   body("password").trim().isLength({ min: 5 }),
   body("username").trim().isLength({ min: 3 }),
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     console.log("errors bhai", errors);
     if(!errors.isEmpty()){
-      return res.status(400).json({errors: errors.array(), message: 'Invalid data'}); 
+      return res.status(400).json({
+        errors: errors.array(), 
+        message: 'Invalid data'
+      }); 
     }
-    console.log("here is the data in requesrt", req.body);
-    res.send("User registered");
+
+    const {email, username, password} = req.body;
+    
+    const newUser = await userModel.create({
+      email,
+      username,
+      password
+    })
+
+    res.json(newUser);
+
+    console.log("here is the data in request", req.body);
+    //  res.send("User registered");
   }
 );
 
